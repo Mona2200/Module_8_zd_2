45435b0af74f1fece30042d8c23401b5315525e8
Алёна Мелехова <alenamelehova13@gmail.com>
2021-11-17 21:43:56 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace ZD2
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine(DirectorySize(@"C:\Users\Алёна\Documents"));
+      }
+
+      static long DirectorySize(string Path)
+      {
+         if (Directory.Exists(Path))
+         {
+
+            try
+            {
+
+               long size = 0;
+
+               string[] files = Directory.GetFiles(Path);
+
+               foreach (var f in files)
+               {
+                  size += f.Length;
+               }
+
+               string[] dirs = Directory.GetDirectories(Path);
+
+               foreach (string d in dirs)
+               {
+                  DirectoryInfo NowDir = new DirectoryInfo(d);
+
+                  size += DirectorySize(d);
+
+               }
+               return size;
+               
+            }
+
+            catch (Exception ex)
+            {
+               Console.WriteLine(ex.Message);
+            }
+
+         }
+         return 0;
+      }
+   }
+}
